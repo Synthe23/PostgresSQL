178b0ac2f9f33d5c5ba6e432c10009b3d4fc5bd2
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-19 09:18:02 +0530

    Created the schema for the student table

---
+++ Part_1/02_Schema.sql
@@ -0,0 +1,12 @@
+-- db -> schema -> table -> rows
+
+-- If not exists it is going to prevent an error if the schema is already existing
+CREATE SCHEMA IF NOT EXISTS basics;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- query
+
+SELECT schema_name
+FROM information_schema.schemata
+ORDER BY schema_name;
