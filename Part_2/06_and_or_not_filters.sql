57c97baef4f004a51e6e94095ee3213f94a8acec
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-22 21:30:13 +0530

    Solved some qns from not

--- Part_2/06_and_or_not_filters.sql
+++ Part_2/06_and_or_not_filters.sql
@@ -5,5 +5,24 @@
 -- 1> Products where the price > 2000 and also category shall be electronics.
 SELECT name, price, category FROM Products WHERE category = 'electronics' and price > 2000;
 
--- 2> Count the number of products under category of electronics
+-- 2> Count the number of products under category of electronics.
 SELECT COUNT(*) FROM Products WHERE category = 'electronics';
+
+-- 3> Products where the category is electronics or furniture.
+SELECT name, category, price FROM Products WHERE category = 'electronics' OR category = 'furniture';
+
+-- 4> Products not in the stationary category
+SELECT name, price, category FROM Products WHERE category NOT IN ('stationery');
+
+-- NECESSORY SYNTAX FOR THE NULL:
+-- One value
+-- WHERE category = 'electronics';
+
+-- Multiple values
+-- WHERE category IN ('electronics', 'furniture');
+
+-- Exclude one value
+-- WHERE category <> 'stationery';
+
+-- Exclude multiple values
+-- WHERE category NOT IN ('stationery', 'furniture');
