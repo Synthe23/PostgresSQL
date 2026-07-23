6cdc57226c34b1d5e8ae0e6477ab3d62728c6412
OmPrakash23 <mishraomm5000@gmail.com>
2026-07-23 20:18:12 +0530

    Solved a problem on the ilike and or query

--- Part_2/07_Like_ilike_patterns.sql
+++ Part_2/07_Like_ilike_patterns.sql
@@ -2,6 +2,31 @@
 -- ilike - case insensitive pattern nmatch
 -- % - means any no of characters
 -- exactly one char
-
 -- the % after the wireless mean anything can come after it
-SELECT name, price FROM Products WHERE name LIKE 'Wireless%';
+-- SELECT
+--     name,
+--     price
+-- FROM
+--     Products
+-- WHERE
+--     name LIKE 'Wireless%';
+
+-- ilike is case insensituve so it matches with desk, DESK, Desk etc
+-- SELECT
+--     name,
+--     category,
+--     price
+-- FROM
+--     Products
+-- WHERE
+--     name ILIKE '%desk%';
+
+-- PSQL query to use ILIKE and find chair in decription OR name and return the searcgh
+SELECT
+    name,
+    description,
+    price,
+    category FROM Products
+WHERE
+    name ILIKE '%chair%'
+    OR description ILIKE '%chair%';
